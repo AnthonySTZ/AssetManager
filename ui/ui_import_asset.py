7ff517abb238231b28a80944fbebabb8ce1dbf76
AnthonySTZ <anthonycandy2309@gmail.com>
2024-06-11 12:29:19 +0200

    add ressource qrc file

--- ui/ui_import_asset.py
+++ ui/ui_import_asset.py
@@ -18,6 +18,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QComboBox, QDialog, QGridLayout,
     QHBoxLayout, QLabel, QPushButton, QSizePolicy,
     QTextEdit, QWidget)
+import ressources_rc
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -28,33 +29,10 @@ class Ui_Dialog(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label = QLabel(Dialog)
-        self.label.setObjectName(u"label")
-        self.label.setAlignment(Qt.AlignCenter)
+        self.cancel_btn = QPushButton(Dialog)
+        self.cancel_btn.setObjectName(u"cancel_btn")
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-
-        self.name_te = QTextEdit(Dialog)
-        self.name_te.setObjectName(u"name_te")
-        self.name_te.setMaximumSize(QSize(16777215, 25))
-        self.name_te.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.name_te.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-        self.gridLayout.addWidget(self.name_te, 0, 1, 1, 2)
-
-        self.label_2 = QLabel(Dialog)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setAlignment(Qt.AlignCenter)
-
-        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
-
-        self.path_te = QTextEdit(Dialog)
-        self.path_te.setObjectName(u"path_te")
-        self.path_te.setMaximumSize(QSize(16777215, 25))
-        self.path_te.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.path_te.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-        self.gridLayout.addWidget(self.path_te, 1, 1, 1, 2)
+        self.gridLayout.addWidget(self.cancel_btn, 3, 0, 1, 2)
 
         self.label_3 = QLabel(Dialog)
         self.label_3.setObjectName(u"label_3")
@@ -68,15 +46,56 @@ class Ui_Dialog(object):
 
         self.gridLayout.addWidget(self.material_cb, 2, 1, 1, 2)
 
-        self.cancel_btn = QPushButton(Dialog)
-        self.cancel_btn.setObjectName(u"cancel_btn")
-
-        self.gridLayout.addWidget(self.cancel_btn, 3, 0, 1, 2)
-
         self.accept_btn = QPushButton(Dialog)
         self.accept_btn.setObjectName(u"accept_btn")
 
         self.gridLayout.addWidget(self.accept_btn, 3, 2, 1, 1)
+
+        self.label_2 = QLabel(Dialog)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.path_te = QTextEdit(Dialog)
+        self.path_te.setObjectName(u"path_te")
+        self.path_te.setMaximumSize(QSize(16777215, 25))
+        self.path_te.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.path_te.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.gridLayout.addWidget(self.path_te, 1, 1, 1, 2)
+
+        self.name_te = QTextEdit(Dialog)
+        self.name_te.setObjectName(u"name_te")
+        self.name_te.setMaximumSize(QSize(16777215, 25))
+        self.name_te.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.name_te.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.gridLayout.addWidget(self.name_te, 0, 1, 1, 2)
+
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setMaximumSize(QSize(25, 25))
+        self.pushButton.setCursor(QCursor(Qt.PointingHandCursor))
+        self.pushButton.setAutoFillBackground(False)
+        self.pushButton.setStyleSheet(u"QPushButton{\n"
+"	background: transparent;\n"
+"\n"
+"}")
+        icon = QIcon()
+        icon.addFile(u":/icons/ui/ressources/folder.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QSize(25, 25))
+        self.pushButton.setAutoDefault(False)
+        self.pushButton.setFlat(True)
+
+        self.gridLayout.addWidget(self.pushButton, 1, 3, 1, 1)
 
 
         self.horizontalLayout.addLayout(self.gridLayout)
@@ -89,12 +108,13 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
-        self.label.setText(QCoreApplication.translate("Dialog", u"Name :", None))
-        self.label_2.setText(QCoreApplication.translate("Dialog", u"Path :", None))
+        self.cancel_btn.setText(QCoreApplication.translate("Dialog", u"Cancel", None))
         self.label_3.setText(QCoreApplication.translate("Dialog", u"Material :", None))
         self.material_cb.setItemText(0, QCoreApplication.translate("Dialog", u"None", None))
 
-        self.cancel_btn.setText(QCoreApplication.translate("Dialog", u"Cancel", None))
         self.accept_btn.setText(QCoreApplication.translate("Dialog", u"Accept", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"Path :", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"Name :", None))
+        self.pushButton.setText("")
     # retranslateUi
 
